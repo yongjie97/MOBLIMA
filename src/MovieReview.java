41e6baad76fbcec9f3ec0c415cfec68c1e71a5d7
Awon001 <101681610+Awon001@users.noreply.github.com>
2022-10-23 15:51:08 +0800

    Add files via upload

---
+++ src/MovieReview.java
@@ -0,0 +1,14 @@
+
+public class MovieReview extends User {
+	
+	private double Rating;
+	
+	public static double getRating() {
+		return this.rating;
+	}
+	public static getReviewer() {
+		return this.name;
+	}
+
+
+}
