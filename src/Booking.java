d4e21203433424c60af2447bd6a3dd5185423cba
lwcwongl <105587401+lwcwongl@users.noreply.github.com>
2022-11-01 18:31:41 +0800

    Update Booking.java

--- src/Booking.java
+++ src/Booking.java
@@ -1,151 +1,147 @@
 	import java.util.Scanner;
-	import java.math.BigDecimal;
-	import java.util.UUID;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.UUID;
+
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;  
+
+import java.io.Serializable;
+import java.util.ArrayList;
+public class Booking implements Serializable{
+	private Customer customer;
+	private Movie movie;
+	private ShowTime time;
+	private int seatno;
+	private double price;
+	private String cinemaname;
+	private int transID;
+	private int age; 
+	private String emailaddress;
+	private int mobilenumber;
 	
-
-	import java.time.LocalDateTime;
-	import java.time.format.DateTimeFormatter;  
-
-
-	public class Booking {
-			public static void main(String[] args) {
-			
-				Scanner sc = new Scanner(System.in);{
-				String s  = "TransactionID : ";
-				BigDecimal  totalPrice=null;
-				BigDecimal normalPrice = new BigDecimal("13.50");
-				BigDecimal seniorPrice = new BigDecimal("5.00");
-				BigDecimal childPrice  = new BigDecimal("7.50");
-				BigDecimal couplePrice = new BigDecimal("17.00");
-				LocalDateTime DateTime = LocalDateTime.now();  
-		        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");  
-		        String formatDateTime = DateTime.format(format);  
-				String bookingDate = formatDateTime;
-				String ShowTime;
-			    Object user;
-			    
-				int[] SeatNo = new int [56];
-				int[] SeatforCouples= new int [12];
-				int SeatsforCouples = 1;
-				int Seats;
-				String Name = null;
-				user= Name;
-				
-				
-			
-				
-			
-				while (true) {
-					System.out.print("Welcome to MOvie Booking and LIsting Management Application(MOBLIMA)\n What is your name?\n");
-					Name= sc.nextLine();
-					 System.out.println();
-					 System.out.println("What is your age?");
-				        int age = sc.nextInt();
-				        System.out.println("Do you want a couple seat?(Type 1 = Yes; Type 2 = No)");
-				        int YesorNo = sc.nextInt();
-				        System.out.println("What is the time of the movie?(in 24-hour format)");
-				         double showTime = sc.nextFloat();
-				        System.out.println("What is the name of the movie?");
-				        String movie = sc.next();
-					System.out.printf("Welcome %s! Please take a look at the seating plan.\n\n", Name);
-					for (int i=1; i<= 40; i++) {
-						System.out.print("-");
-					}
-					System.out.println();
-					
-					System.out.print(" CINEMA SEATING LAYOUT");
-					System.out.println();
-					
-					for (int j =1; j<=40; j ++) {
-						System.out.print("-");
-					}
-					System.out.println();
-					
-					for (int j = 1; j <= 40; j++) {
-				        System.out.print("-");
-				      }
-				      System.out.println();
-	                  
-				      for (int SeatCounter = 1; SeatCounter < SeatNo.length; SeatCounter++) {
-				        System.out.printf(SeatCounter + "\t");
-
-				        if( (SeatCounter) % 11 == 0 )
-				        	System.out.println();
-				      }
-				      System.out.println();
-				      System.out.print("Couple Seats");
-				      System.out.println();
-				      
-				for (int SeatCounter1 = 1; SeatCounter1 < SeatforCouples.length;SeatCounter1++) {
-					System.out.printf(SeatCounter1 + "\t");
-					   if( (SeatCounter1) % 11 == 0 )
-				        	System.out.println();
-					
-				}
-				 System.out.println();
-				 
-				for (int k =1; k<=40; k ++) {
-					System.out.print("-");
-				}
-				System.out.println();
-			      System.out.print("Which seat would you like to book? ");
-			      Seats = sc.nextInt();
-
-			      while (Seats < 0 || Seats > 55) {
-			        System.out.println("Only Seats 1 to 55 are allowed to be booked. Please try again: ");
-			        Seats = sc.nextInt();
-			      }
-			      System.out.println("For One Person Only:");
-				        if (age < 12) { 
-				            totalPrice = childPrice;
-				        System.out.println("You are under 12." + "Your price is: " + totalPrice);}
-				        
-			
-				        else {
-				        	if (age >= 12 && age <= 65) {
-				            totalPrice = normalPrice;
-				                System.out.println("Your price is SGD " + totalPrice);}
-				                else if (age >65) {
-				            totalPrice = seniorPrice;
-				                 System.out.println("You are over 65. Your price is SGD " + totalPrice);}}
-				        	
-				        System.out.println("For Couple:");
-				      if (YesorNo < 2 ){
-				       totalPrice = couplePrice;
-				       System.out.println("Your price is SGD " + totalPrice);}
-				      else {
-				              if(YesorNo>=2) {
-				    		  System.out.println("Not Applicable");}
-				      }
-				    	  
-				
-				
-				    
-				     
-				        for (int SeatCounter = 0; SeatCounter < SeatNo.length; SeatCounter++) {
-					        if (SeatCounter == Seats) {
-					          System.out.println("Seat " + Seats + " is successfully booked.");}
-				        }
-				        
-				        UUID TransactionID =UUID.randomUUID();    
-				        
-				        
-				        
-				     
-				    	  
-				        System.out.printf("ShowTime : "+"%.2f"+"HRS",showTime);
-				        System.out.println();
-				        System.out.println("Movie Title : " + movie);
-				        System.out.println("Name : " + Name);
-				        System.out.println("Booking Date and Timing : " + bookingDate);
-				        System.out.println(s + TransactionID); 
-				        System.out.println("Thanks for Booking with Us!");
-				        
-			}
-				
-			}
-			}
+	public Booking(Customer customer, Movie movie, ShowTime time, int seatno, double price, int transID, String cinemaname,int mobilenumber,String emailaddress) {
+		this.customer = customer;
+		this.time = time;
+		this.movie = movie;
+		this.seatno = seatno;
+		this.price = price;
+		this.transID = transID;
+		this.cinemaname = cinemaname;
 	}
+	
+	public String getName() {
+		return customer.getName();
+	}
+	
+	public String getMovieName() {
+		return movie.getName();
+	}
+	
+	public LocalDateTime getShowTime() {
+		return time.getDateTime();
+	}
+	public int getMobileNumber() {
+		return mobilenumber;
+	}
+	public int getSeat() {
+		return seatno;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	public int getID() {
+		return transID;
+	}
+	public String getEmailAddress() {
+		return emailaddress;
+	}
+	public String getCinema() {
+		return cinemaname;
+	}
+		
+			BigDecimal  TotalPrice=null;
+			BigDecimal NormalPrice = new BigDecimal("13.50");
+			BigDecimal SeniorPrice = new BigDecimal("5.00");
+			BigDecimal ChildPrice  = new BigDecimal("7.50");
+			BigDecimal PlatinumPrice = new BigDecimal("15.00");
+			LocalDateTime DateTime = LocalDateTime.now();  
+	        DateTimeFormatter Format = DateTimeFormatter.ofPattern("YYYYMMDDHHmm");  
+	        String FormatDateTime = DateTime.format(Format);  
+		
+			
+			 
+			BigDecimal WeekendPriceIncrementpercent =new BigDecimal("0.40");
+		    BigDecimal HolidayPriceIncrement = new BigDecimal("2.00");
+		    BigDecimal GoodsandServicesTaxpercent= new BigDecimal("0.7");
+			int k = 0;
+			int Saturday=1;
+			int Sunday=2;
+			ArrayList<String> Holiday = new ArrayList <String>();
+			ArrayList<String> s = Holiday; 
+			
+			{
+			
+		    if (Holiday != null) {
+							TotalPrice = NormalPrice.add(HolidayPriceIncrement);
+							TotalPrice = SeniorPrice.add(HolidayPriceIncrement);
+							TotalPrice = ChildPrice.add(HolidayPriceIncrement);
+							TotalPrice = PlatinumPrice.add(HolidayPriceIncrement);
+						 }
+		    else {
+				             if (k<=3) {
+							BigDecimal WeekendPriceIncrement = NormalPrice.multiply(WeekendPriceIncrementpercent);
+							BigDecimal WeekendPriceIncrement2 = PlatinumPrice.multiply(WeekendPriceIncrementpercent);
+							BigDecimal WeekendPriceIncrement3 = SeniorPrice.multiply(WeekendPriceIncrementpercent);
+							BigDecimal WeekendPriceIncrement4 = ChildPrice.multiply(WeekendPriceIncrementpercent);
+
+						    WeekendPriceIncrement = WeekendPriceIncrement.setScale(2, RoundingMode.HALF_UP);
+						    WeekendPriceIncrement2 = WeekendPriceIncrement2.setScale(2, RoundingMode.HALF_UP);
+						    WeekendPriceIncrement3 = WeekendPriceIncrement3.setScale(2, RoundingMode.HALF_UP);
+						    WeekendPriceIncrement4 = WeekendPriceIncrement4.setScale(2, RoundingMode.HALF_UP);
+
+						     TotalPrice = NormalPrice.add(WeekendPriceIncrement);
+						     TotalPrice = PlatinumPrice.add(WeekendPriceIncrement2);
+						     TotalPrice = SeniorPrice.add(WeekendPriceIncrement3);
+						     TotalPrice = ChildPrice.add(WeekendPriceIncrement4);}
+				              else if (k>=3) {
+								 BigDecimal GoodsandServicesTax = NormalPrice.multiply(GoodsandServicesTaxpercent);
+								 BigDecimal GoodsandServicesTax2 = PlatinumPrice.multiply(GoodsandServicesTaxpercent);
+								 BigDecimal GoodsandServicesTax3 = SeniorPrice.multiply(GoodsandServicesTaxpercent);
+								 BigDecimal GoodsandServicesTax4 = ChildPrice.multiply(GoodsandServicesTaxpercent);
+					             
+								GoodsandServicesTax = GoodsandServicesTax.setScale(2, RoundingMode.HALF_UP);
+								  
+								GoodsandServicesTax2 = GoodsandServicesTax2.setScale(2, RoundingMode.HALF_UP);
+								  
+								GoodsandServicesTax3 = GoodsandServicesTax3.setScale(2, RoundingMode.HALF_UP);
+								  
+								GoodsandServicesTax4 = GoodsandServicesTax4.setScale(2, RoundingMode.HALF_UP);
+						     
+								 TotalPrice = NormalPrice.add(GoodsandServicesTax);
+							     TotalPrice = PlatinumPrice.add(GoodsandServicesTax2);
+							     TotalPrice = SeniorPrice.add(GoodsandServicesTax3);
+							     TotalPrice = ChildPrice.add(GoodsandServicesTax4);
+				             }
+						 }
+			        System.out.println("ShowTime:" + time);
+			        System.out.println("Movie Title : " + movie);
+			        System.out.println("Name : " + customer);
+			        System.out.println("Cinema Name:"+cinemaname);
+			        System.out.println("Seat No.:"+ seatno);
+			        System.out.println("Total Price:" + TotalPrice);
+			        System.out.println("Mobile Number:"+ mobilenumber);
+			        System.out.println("Email Address:"+ emailaddress);
+			        System.out.println("Booking Date:"+ FormatDateTime);
+			        System.out.println("Transaction ID: MOB"+FormatDateTime); 
+			        System.out.println("Thanks for Booking with Us!");
+			        
+		
+}
+}
+			
 
 
 
