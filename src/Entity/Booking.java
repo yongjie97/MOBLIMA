98e14f40de57e34f5675e255d3016b6b1b988d2f
lwcwongl <105587401+lwcwongl@users.noreply.github.com>
2022-11-02 10:05:18 +0800

    Update Booking.java

--- src/Entity/Booking.java
+++ src/Entity/Booking.java
@@ -1,50 +1,66 @@
 package Entity;
 import java.io.Serializable;
 import java.time.LocalDateTime;
-
-public class Booking implements Serializable{
-	private Customer customer;
-	private Movie movie;
-	private ShowTime time;
-	private int seatno;
-	private double price;
-	private String cinemaname;
-	private int transID;
-	public Booking(Customer customer, Movie movie, ShowTime time, int seatno, double price, int transID, String cinemaname) {
-		this.customer = customer;
-		this.time = time;
-		this.movie = movie;
-		this.seatno = seatno;
-		this.price = price;
-		this.transID = transID;
-		this.cinemaname = cinemaname;
-	}
-	
-	public String getName() {
-		return customer.getName();
-	}
-	
-	public String getMovieName() {
-		return movie.getName();
-	}
-	
-	public LocalDateTime getShowTime() {
-		return time.getDateTime();
-	}
-	
-	public int getSeat() {
-		return seatno;
-	}
-	
-	public double getPrice() {
-		return price;
-	}
-	public int getID() {
-		return transID;
-	}
-	
-	public String getCinema() {
-		return cinemaname;
-	}
-	
-}	
+import java.util.Scanner;
+import java.time.format.DateTimeFormatter;  
+import java.util.ArrayList;
+	public class Booking implements Serializable{
+		private Customer customer;
+		private Movie movie;
+		private ShowTime time;
+		private int seatno;
+		private String cinemaname;
+		private String transID; 
+		private String emailaddress;
+		private int mobilenumber;
+		private String bookingdate;
+		private BigDecimal TotalPrice;
+		
+		public Booking(Customer customer, Movie movie, ShowTime time, int seatno, BigDecimal TotalPrice, String transID, String cinemaname,int mobilenumber,String emailaddress,String bookingdate) {
+			this.customer = customer;
+			this.time = time;
+			this.movie = movie;
+			this.seatno = seatno;
+			this.TotalPrice = TotalPrice;
+			this.transID = transID;
+			this.cinemaname = cinemaname;
+			this.mobilenumber = mobilenumber;
+			this.emailaddress = emailaddress;
+			this.bookingdate = bookingdate;		
+		}
+		
+		public String getName() {
+			return customer.getName();
+		}
+		
+		public String getMovieName() {
+			return movie.getName();
+		}
+		
+		public LocalDateTime getShowTime() {
+			return time.getDateTime();
+		}
+		public int getMobileNumber() {
+			return mobilenumber;
+		}
+		public int getSeat() {
+			return seatno;
+		}
+		
+		public BigDecimal getTotalPrice() {
+			return TotalPrice;
+		}
+		public String getID() {
+			return transID;
+		}
+		public String getEmailAddress() {
+			return emailaddress;
+		}
+		public String getCinema() {
+			return cinemaname;
+		}
+		public String getBookingDate() {
+			return bookingdate;
+		}
+		}
+			
