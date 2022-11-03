2786f4cb0e0fd198f328c34e3cf2084f8fdca25e
lwcwongl <105587401+lwcwongl@users.noreply.github.com>
2022-11-03 22:47:39 +0800

    Update systemSettingsController.java

--- src/Controller/systemSettingsController.java
+++ src/Controller/systemSettingsController.java
@@ -1,19 +1,43 @@
 package Controller;
 import Constant.DataFileConstant;
 import Entity.systemSettings;
-import Repository.systemSettingsRepository;
+import Repository.SystemRepository;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 public class systemSettingsController {
-	private static systemSettingsRespository systemSettingsRepository = new systemSettingsRespository(DataFileConstant.SYSTEMSETTINGS_FILE);
+	private static Repository.SystemRepository SystemRepository = new Repository.SystemRepository(DataFileConstant.SYSTEMSETTINGS_FILE);
 	
 	
-	public static void addsystemSettings(ArrayList<String> holiday, BigDecimal totalPrice, BigDecimal normalPrice, BigDecimal seniorPrice ,BigDecimal childPrice, BigDecimal platinumPrice, BigDecimal weekendPriceIncrementpercent, BigDecimal holidayPriceIncrement, BigDecimal goodsandServicesTaxpercentforWeekdays)
-	{ systemSettings systemSettings = new systemSettings(totalPrice,normalPrice,seniorPrice,childPrice,platinumPrice,weekendPriceIncrementpercent, holidayPriceIncrement,goodsandServicesTaxpercentforWeekdays);
+	public static void addsystemSettings(ArrayList<String> holiday, BigDecimal totalPrice, BigDecimal regularPrice, BigDecimal seniorPrice ,BigDecimal childPrice, BigDecimal platinumPrice, BigDecimal weekendPriceIncrementpercent, BigDecimal holidayPriceIncrement, BigDecimal goodsandServicesTaxpercentforWeekdays)
+	{ systemSettings systemSettings = new systemSettings(totalPrice,regularPrice,seniorPrice,childPrice,platinumPrice,weekendPriceIncrementpercent, holidayPriceIncrement,goodsandServicesTaxpercentforWeekdays);
 	systemSettings.add(systemSettings);
+	}
+
+
+	public static char[] getticketPrices(BigDecimal normalPrice, BigDecimal seniorPrice, BigDecimal childPrice,
+			BigDecimal platinumPrice, BigDecimal couplePrice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public static char[] getweekendPriceIncrementpercent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public static char[] getgoodsandServicesTaxpercentforWeekdays() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public static char[] getholidayPriceIncrement() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 		
 
 }
-
