f3aeb2b5aa2e5de21de745a19fd63c1b3c4e9fa7
lwcwongl <105587401+lwcwongl@users.noreply.github.com>
2022-11-03 19:42:35 +0800

    Update BookingBoundary.java

--- src/Boundary/BookingBoundary.java
+++ src/Boundary/BookingBoundary.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import Controller.BookingController
+import Controller.BookingController;
 
 import Controller.CinemaController;
 import Controller.ShowTimeController;
