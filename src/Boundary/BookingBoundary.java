50884eca1f3e89b3ffdc419c5af1bc5c537c74f5
lwcwongl <105587401+lwcwongl@users.noreply.github.com>
2022-11-03 17:35:22 +0800

    Add files via upload

--- src/Boundary/BookingBoundary.java
+++ src/Boundary/BookingBoundary.java
@@ -1,13 +1,17 @@
-package Boundary;
+package sce.SC2002.WWC.first;
+
 import java.util.Scanner;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.lang.Math;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import Controller.BookingController
 
@@ -21,39 +25,6 @@ import Entity.Booking;
 import Entity.Cinema;
 
 public class BookingBoundary {
-	private static Customer customer;
-	private static Movie movie;
-	private static ShowTime time;
-	private static int seatno;
-	private static String cinemaname;
-	private String transID;
-	private static String emailaddress;
-	private static int mobilenumber;
-	private String bookingdate;
-	private static BigDecimal TotalPrice;
-	static BigDecimal CouplePrice = new BigDecimal("16.00");
-	static BigDecimal NormalPrice = new BigDecimal("13.50");
-	static BigDecimal SeniorPrice = new BigDecimal("5.00");
-	static BigDecimal ChildPrice = new BigDecimal("7.50");
-	static BigDecimal PlatinumPrice = new BigDecimal("15.00");
-	static BigDecimal WeekendPriceIncrementpercent = new BigDecimal("0.40");
-	static BigDecimal HolidayPriceIncrement = new BigDecimal("2.00");
-	static BigDecimal GoodsandServicesTaxpercentforWeekdays = new BigDecimal("0.7");
-
-	public void Booking(Customer customer, Movie movie, ShowTime time, int seatno, BigDecimal TotalPrice,
-			String transID, String cinemaname, int mobilenumber, String emailaddress, String bookingdate) {
-		this.customer = customer;
-		this.time = time;
-		this.movie = movie;
-		this.seatno = seatno;
-		this.TotalPrice = TotalPrice;
-		this.transID = transID;
-		this.cinemaname = cinemaname;
-		this.mobilenumber = mobilenumber;
-		this.emailaddress = emailaddress;
-		this.bookingdate = bookingdate;
-	}
-
 	public static void Booking() {
 		int userInput = 0;
 
@@ -148,13 +119,14 @@ public class BookingBoundary {
 		System.out.print("Please enter a positive integer (0 for holidays,1 for weekends, others for other days): ");
 		int choice = s.nextInt();
 
-		while (true) {
-			if (choice == 0) {
-				TotalPrice = NormalPrice.add(HolidayPriceIncrement);
-				TotalPrice = SeniorPrice.add(HolidayPriceIncrement);
-				TotalPrice = ChildPrice.add(HolidayPriceIncrement);
-				TotalPrice = PlatinumPrice.add(HolidayPriceIncrement);
-				TotalPrice = CouplePrice.add(HolidayPriceIncrement);
+		List<List<String>> rows = new ArrayList<>();
+		List<String> headers = Arrays.asList("        ", "Weekday", "Weekend", "Holiday");
+		rows.add(headers);
+		rows.add(Arrays.asList("Child", TotalPrice = ChildPrice.add(GoodsandServicesTax4),TotalPrice = ChildPrice.add(weekendPriceIncrement4),TotalPrice = ChildPrice.add(HolidayPriceIncrement)));
+		rows.add(Arrays.asList("Senior", TotalPrice = SeniorPrice.add(GoodsandServicesTax3), TotalPrice = SeniorPrice.add(weekendPriceIncrement3), TotalPrice = SeniorPrice.add(HolidayPriceIncrement)));
+		rows.add(Arrays.asList("Normal", TotalPrice = NormalPrice.add(GoodsandServicesTax), TotalPrice = NormalPrice.add(weekendPriceIncrement), TotalPrice = NormalPrice.add(HolidayPriceIncrement)));
+		rows.add(Arrays.asList("Couple", TotalPrice = CouplePrice.add(GoodsandServicesTax5), TotalPrice = CouplePrice.add(weekendPriceIncrement5), TotalPrice = CouplePrice.add(HolidayPriceIncrement)));
+		System.out.println(formatAsTable(rows));
 				if (age < 12) {
 					TotalPrice = ChildPrice;
 					System.out.println("You are under 12." + "Your price is: " + TotalPrice);
@@ -187,116 +159,34 @@ public class BookingBoundary {
 						System.out.println("Not Applicable");
 					}
 				}
-			} else {
-				if (choice == 1) {
-					BigDecimal weekendPriceIncrement = NormalPrice.multiply(WeekendPriceIncrementpercent);
-					BigDecimal weekendPriceIncrement2 = PlatinumPrice.multiply(WeekendPriceIncrementpercent);
-					BigDecimal weekendPriceIncrement3 = SeniorPrice.multiply(WeekendPriceIncrementpercent);
-					BigDecimal weekendPriceIncrement4 = ChildPrice.multiply(WeekendPriceIncrementpercent);
-					BigDecimal weekendPriceIncrement5 = CouplePrice.multiply(WeekendPriceIncrementpercent);
+			
+	}
 
-					weekendPriceIncrement = weekendPriceIncrement.setScale(2, RoundingMode.HALF_UP);
-					weekendPriceIncrement2 = weekendPriceIncrement2.setScale(2, RoundingMode.HALF_UP);
-					weekendPriceIncrement3 = weekendPriceIncrement3.setScale(2, RoundingMode.HALF_UP);
-					weekendPriceIncrement4 = weekendPriceIncrement4.setScale(2, RoundingMode.HALF_UP);
-					weekendPriceIncrement5 = weekendPriceIncrement5.setScale(2, RoundingMode.HALF_UP);
+	private static String formatAsTable(List<List<String>> rows) {
+		{
+		    int[] maxLengths = new int[rows.get(0).size()];
+		    for (List<String> row : rows)
+		    {
+		        for (int i = 0; i < row.size(); i++)
+		        {
+					maxLengths[i] = Math.max(maxLengths[i], row.get(i).length());
+		        }
+		    }
 
-					TotalPrice = NormalPrice.add(weekendPriceIncrement);
-					TotalPrice = PlatinumPrice.add(weekendPriceIncrement2);
-					TotalPrice = SeniorPrice.add(weekendPriceIncrement3);
-					TotalPrice = ChildPrice.add(weekendPriceIncrement4);
-					TotalPrice = CouplePrice.add(weekendPriceIncrement5);
-					if (age < 12) {
-						TotalPrice = ChildPrice;
-						System.out.println("You are under 12." + "Your price is: " + TotalPrice);
-					}
+		    StringBuilder formatBuilder = new StringBuilder();
+		    for (int maxLength : maxLengths)
+		    {
+		        formatBuilder.append("%-").append(maxLength + 2).append("s");
+		    }
+		    String format = formatBuilder.toString();
 
-					else {
-						if (age >= 12 && age <= 65) {
-							TotalPrice = NormalPrice;
-							System.out.println("Your price is SGD " + TotalPrice);
-						} else if (age > 65) {
-							TotalPrice = SeniorPrice;
-							System.out.println("You are over 65. Your price is SGD " + TotalPrice);
-						}
-					}
-					System.out.println("For Upgraded Seats :");
-					if (YesorNo < 2) {
-						TotalPrice = PlatinumPrice;
-						System.out.println("Your price is SGD " + TotalPrice);
-					} else {
-						if (YesorNo >= 2) {
-							System.out.println("Not Applicable");
-						}
-					}
-					System.out.println("For Couple Seats:");
-					if (YorN < 2) {
-						TotalPrice = CouplePrice;
-						System.out.println("Your price is SGD" + TotalPrice);
-					} else {
-						if (YorN > 2) {
-							System.out.println("Not Applicable");
-						}
-					}
-				} else if (choice > 1) {
-					BigDecimal GoodsandServicesTax = NormalPrice.multiply(GoodsandServicesTaxpercentforWeekdays);
-					BigDecimal GoodsandServicesTax2 = PlatinumPrice.multiply(GoodsandServicesTaxpercentforWeekdays);
-					BigDecimal GoodsandServicesTax3 = SeniorPrice.multiply(GoodsandServicesTaxpercentforWeekdays);
-					BigDecimal GoodsandServicesTax4 = ChildPrice.multiply(GoodsandServicesTaxpercentforWeekdays);
-					BigDecimal GoodsandServicesTax5 = CouplePrice.multiply(GoodsandServicesTaxpercentforWeekdays);
-
-					GoodsandServicesTax = GoodsandServicesTax.setScale(2, RoundingMode.HALF_UP);
-
-					GoodsandServicesTax2 = GoodsandServicesTax2.setScale(2, RoundingMode.HALF_UP);
-
-					GoodsandServicesTax3 = GoodsandServicesTax3.setScale(2, RoundingMode.HALF_UP);
-
-					GoodsandServicesTax4 = GoodsandServicesTax4.setScale(2, RoundingMode.HALF_UP);
-
-					GoodsandServicesTax5 = GoodsandServicesTax5.setScale(2, RoundingMode.HALF_UP);
-
-					TotalPrice = NormalPrice.add(GoodsandServicesTax);
-					TotalPrice = PlatinumPrice.add(GoodsandServicesTax2);
-					TotalPrice = SeniorPrice.add(GoodsandServicesTax3);
-					TotalPrice = ChildPrice.add(GoodsandServicesTax4);
-					TotalPrice = CouplePrice.add(GoodsandServicesTax5);
-
-					if (age < 12) {
-						TotalPrice = ChildPrice;
-						System.out.println("You are under 12." + "Your price is: " + TotalPrice);
-					}
-
-					else {
-						if (age >= 12 && age <= 65) {
-							TotalPrice = NormalPrice;
-							System.out.println("Your price is SGD " + TotalPrice);
-						} else if (age > 65) {
-							TotalPrice = SeniorPrice;
-							System.out.println("You are over 65. Your price is SGD " + TotalPrice);
-						}
-					}
-					System.out.println("For Upgraded Seats :");
-					if (YesorNo < 2) {
-						TotalPrice = PlatinumPrice;
-						System.out.println("Your price is SGD " + TotalPrice);
-					} else {
-						if (YesorNo >= 2) {
-							System.out.println("Not Applicable");
-						}
-					}
-					System.out.println("For Couple Seats:");
-					if (YorN < 2) {
-						TotalPrice = CouplePrice;
-						System.out.println("Your price is SGD" + TotalPrice);
-					} else {
-						if (YorN > 2) {
-							System.out.println("Not Applicable");
-						}
-					}
-				}
-			}
+		    StringBuilder result = new StringBuilder();
+		    for (List<String> row : rows)
+		    {
+		        result.append(String.format(format, row.toArray(new String[0]))).append("\n");
+		    }
+		    return result.toString();
 		}
-
 	}
 
 	public static void viewTransactionDetails() {
