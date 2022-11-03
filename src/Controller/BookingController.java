85c7803011da787ac2231c0ac825942b977428af
lwcwongl <105587401+lwcwongl@users.noreply.github.com>
2022-11-03 22:46:05 +0800

    Update BookingController.java

--- src/Controller/BookingController.java
+++ src/Controller/BookingController.java
@@ -14,7 +14,23 @@ import Repository.BookingRepository;
 
 
 public class BookingController{
-	private static BigDecimal TotalPrice;
+	private static BookingRepository bookingRepository = new BookingRepository(DataFileConstant.BOOKING_FILE);
+	
+	public static void addBooking(Customer customer, Movie movie,ShowTime showtime, int seatno,double price, int transID, String cinemaname) {
+		Booking Booking = new Booking(customer, movie,  showtime, seatno,price,transID,cinemaname);
+		bookingRepository.add(Booking);
+	}
+	
+	
+	public static void getPrice(Boolean upgraded,Boolean coupleSeat, int age , BigDecimal normalPrice, BigDecimal totalPrice, BigDecimal couplePrice, BigDecimal platinumPrice,BigDecimal seniorPrice, BigDecimal childPrice) {
+		Booking Booking  = new Booking(upgraded, coupleSeat, age , normalPrice, totalPrice, couplePrice, platinumPrice, seniorPrice, childPrice);
+	       bookingRepository.get(upgraded,coupleSeat,age,normalPrice,totalPrice,couplePrice,platinumPrice,seniorPrice,childPrice);
+
+}
+
+
+
+	private static BigDecimal totalPrice;
 	static BigDecimal couplePrice = new BigDecimal("16.00");
 	static BigDecimal normalPrice = new BigDecimal("13.50");
 	static BigDecimal seniorPrice = new BigDecimal("5.00");
@@ -24,15 +40,16 @@ public class BookingController{
 	static BigDecimal holidayPriceIncrement = new BigDecimal("2.00");
 	static BigDecimal goodsandServicesTaxpercentforWeekdays = new BigDecimal("0.7");
 
+     int choice;
 	Scanner s = new Scanner(System.in);
-	int choice = s.nextInt();{
-	while (true) {
+	{
+	
 		if (choice == 0) {
-			totalPrice = normalPrice.add(HolidayPriceIncrement);
-			totalPrice = seniorPrice.add(HolidayPriceIncrement);
-			totalPrice = childPrice.add(HolidayPriceIncrement);
-			totalPrice = platinumPrice.add(HolidayPriceIncrement);
-			totalPrice = couplePrice.add(HolidayPriceIncrement);
+			totalPrice = normalPrice.add(holidayPriceIncrement);
+			totalPrice = seniorPrice.add(holidayPriceIncrement);
+			totalPrice = childPrice.add(holidayPriceIncrement);
+			totalPrice = platinumPrice.add(holidayPriceIncrement);
+			totalPrice = couplePrice.add(holidayPriceIncrement);
 			
 		} else {
 			if (choice == 1) {
@@ -81,21 +98,13 @@ public class BookingController{
 			
 		}
 		}
-	}
-
-}
-		private static BookingRepository bookingRepository = new BookingRepository(DataFileConstant.BOOKING_FILE);
-		
-		
-		
-		public static void addBooking(Customer customer, Movie movie,ShowTime showtime, int seatno,double price, int transID, String cinemaname) {
-			Booking Booking = new Booking(customer, movie,  showtime, seatno,price,transID,cinemaname);
-			bookingRepository.add(Booking);
-		}
 	
-		public static void getPrice(Boolean upgraded,Boolean coupleSeat, int age , BigDecimal normalPrice, BigDecimal totalPrice, BigDecimal couplePrice, BigDecimal platinumPrice,BigDecimal seniorPrice, BigDecimal childPrice) {
-			Booking Booking  = new Booking(upgraded, coupleSeat, age , normalPrice, totalPrice, couplePrice, platinumPrice, seniorPrice, childPrice);
-		       bookingRepository.get(upgraded,coupleSeat,age,normalPrice,totalPrice,couplePrice,platinumPrice,seniorPrice,childPrice);
-		}
-
+	}
+	public static char[] gettransactionDetails(Customer customer, Movie movie, ShowTime time, int seatNo,
+			BigDecimal totalPrice2, String transID, String cinemaName, int mobileNumber, String emailAddress,
+			String bookingDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
+
